77a3636740fcd7d9178389346d2631b931db336b
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-02-04 12:28:32 -0500

    Chapter 3 lesson 2: added soud and effect

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -10,14 +10,23 @@ import AVFAudio
 
 struct ContentView: View {
     @State private var audioPlayer: AVAudioPlayer!
+//    @State private var scale = 1.0
+    @State private var isFullSize = true
+    
     var body: some View {
         VStack {
             Image("clown")
                 .resizable()
                 .scaledToFit()
+                .scaleEffect(isFullSize ? 1.0 : 0.9)
                 .onTapGesture {
                     playSound(soundName: "punchSound")
+                    isFullSize = false
+                    withAnimation (.spring(response: 1.0 , dampingFraction: 0.05) ){
+                        isFullSize = true
+                    }
                 }
+//                .animation(.spring(response: 1.0 , dampingFraction: 0.05) , value: scale)
             
             Spacer(
                 
